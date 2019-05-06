e834954e67bc4d4a24708858ad4aec7abadb744d
Toyz <>
2019-05-06 14:58:34 +0800

    no message

--- src/com/JDBC/PreparedStatement.java
+++ src/com/JDBC/PreparedStatement.java
@@ -2,13 +2,12 @@ package com.JDBC;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
 
 public class PreparedStatement {
 
 	public static void main(String[] args) {
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			Class.forName("com.mysql.jdbc.Driver");
 			System.out.println("数据库加载成功");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
@@ -16,10 +15,10 @@ public class PreparedStatement {
 		String sql = "INSERT INTO user VALUES(null,?,?)";
 		try (
 				Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/mysqlTest?characterEcoding=utf8","root","admin123");
-				Statement s = c.createStatement();
 				)
 		{
-			
+            PreparedStatement ps = (PreparedStatement) c.prepareStatement(sql);
+            
 			
 		} catch (Exception e) {
 			// TODO: handle exception
