fed85f95a03fca72532c0d0b44eae77fd12b16cd
Toyz <>
2019-05-10 19:55:14 +0800

    no message

---
+++ src/com/GUI/TestTable.java
@@ -0,0 +1,39 @@
+package com.GUI;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+/*
+ * 表格
+ */
+public class TestTable {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setBounds(400, 300, 800, 400);
+		frame.setLayout(new BorderLayout());
+		
+		//新建表头和表格项
+		String[] columName = new String[] {"id","name","hp","damage"};
+		String[][] heros = new String[][] {
+			{"1","VN","500","100"},{"2","EZ","500","100"},{"3","Teemo","500","100"}	
+		};
+		
+		//新建表格
+		JTable table = new JTable(heros,columName);
+		
+		//新建滚动面板
+		JScrollPane sp = new JScrollPane();
+		sp.setViewportView(table);
+		frame.add(sp,BorderLayout.CENTER);
+		
+		//窗体设置
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+	}
+
+}
--- src/com/GUI/TestToolBar.java
+++ src/com/GUI/TestToolBar.java
@@ -1,5 +1,9 @@
 package com.GUI;
 
+import java.awt.BorderLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JToolBar;
 
@@ -11,7 +15,29 @@ public class TestToolBar {
 		
 		//新建工具栏
 		JToolBar bar = new JToolBar();
+		//禁止工具栏移动
+		bar.setFloatable(false);
+		//新建按钮
+		JButton but1 = new JButton(new ImageIcon("/Users/toyz/Package/IntermediateJAVA/src/com/GUI/pics/1.jpg"));
+		JButton but2 = new JButton(new ImageIcon("/Users/toyz/Package/IntermediateJAVA/src/com/GUI/pics/2.jpg"));
+		JButton but3 = new JButton(new ImageIcon("/Users/toyz/Package/IntermediateJAVA/src/com/GUI/pics/3.jpg"));
+		JButton but4 = new JButton(new ImageIcon("/Users/toyz/Package/IntermediateJAVA/src/com/GUI/pics/4.jpg"));
+		JButton but5 = new JButton(new ImageIcon("/Users/toyz/Package/IntermediateJAVA/src/com/GUI/pics/5.jpg"));
+		JButton but6 = new JButton(new ImageIcon("/Users/toyz/Package/IntermediateJAVA/src/com/GUI/pics/6.jpg"));
+		but1.setToolTipText("ADC");
+		bar.add(but1);
+		bar.add(but2);
+		bar.add(but3);
+		bar.add(but4);
+		bar.add(but5);
+		bar.add(but6);
 		
+		frame.setLayout(new BorderLayout());
+		frame.add(bar,BorderLayout.NORTH);
+
+		//窗体设置
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 
 }
