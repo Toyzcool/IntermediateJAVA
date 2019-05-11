c3f08a9b27f9f7f4aba4f824d7d15a51921390a5
Toyz <>
2019-05-11 15:44:34 +0800

    no message

--- src/com/Practice/UserManageSystem/UserInterface.java
+++ src/com/Practice/UserManageSystem/UserInterface.java
@@ -1,8 +1,12 @@
 package com.Practice.UserManageSystem;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
@@ -14,12 +18,32 @@ public class UserInterface {
 		frame.setBounds(250, 200, 1000, 500);
 		frame.setLayout(new BorderLayout());
 		
+		/*
+		 * 数据表格 
+		 */
 		//新建tablemodel
 		UserTableModel utm = new UserTableModel();
 		//新增数据展示表格
 		JTable tableDisplay = new JTable(utm);
 		JScrollPane sp = new JScrollPane(tableDisplay);//将表格放入支持滑动的面板
 		
+		//新建按钮模块
+		JPanel panelSet = new JPanel();
+		panelSet.setLayout(new FlowLayout());
+		JButton butAdd = new JButton("新增");
+		JButton butDelete = new JButton("删除");
+		JButton butUpdate = new JButton("编辑");
+		panelSet.add(butAdd);
+		panelSet.add(butDelete);
+		panelSet.add(butUpdate);
+		
+		/*
+		 * 窗体设置
+		 */
+		frame.add(sp,BorderLayout.NORTH);
+		frame.add(panelSet,BorderLayout.SOUTH);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 		
 	}
 
