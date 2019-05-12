02082fad0e3d9cb1b4a40a67e5a36010597ae3de
Toyz <>
2019-05-12 23:32:16 +0800

    no message

--- src/com/Practice/UserManageSystem/UserInterface.java
+++ src/com/Practice/UserManageSystem/UserInterface.java
@@ -53,33 +53,34 @@ public class UserInterface {
 		panelSet.add(butDelete);
 		panelSet.add(butUpdate);
 		
-		//新建弹出框组件
-			//添加窗体和面板
+		/*
+		 * 新建添加弹出框组件
+		 */
+		//添加窗体和面板
 		JFrame addFrame = new JFrame("用户添加");
-		addFrame.setBounds(250, 200, 300, 180);
+		addFrame.setBounds(600, 350, 300, 180);
 		JPanel addPanel = new JPanel();
 		addPanel.setLayout(null);
-
-				//第一行——用户名输入
+		//第一行——用户名输入
 		JLabel labNameAdd = new JLabel("用户名：");
 		labNameAdd.setBounds(20,20, 70, 30);
 		JTextField tfNameAdd = new JTextField();
 		tfNameAdd.setBounds(90, 20, 150, 30);
 		addPanel.add(labNameAdd);
 		addPanel.add(tfNameAdd);
-				//第二行——密码输入
+		//第二行——密码输入
 		JLabel labPasswordAdd = new JLabel("密码：");
 		labPasswordAdd.setBounds(20, 50, 70, 30);
 		JTextField tfPasswordAdd = new JTextField();
 		tfPasswordAdd.setBounds(90, 50, 150, 30);
 		addPanel.add(labPasswordAdd);
 		addPanel.add(tfPasswordAdd);
-				//第三行——提示语
+		//第三行——提示语
 		JLabel labTip = new JLabel();
 		labTip.setBounds(90, 80, 200, 20);
 		labTip.setForeground(Color.RED);;
 		addPanel.add(labTip);
-				//第四行——提交按钮
+		//第四行——提交按钮
 		JButton butSubmitAdd = new JButton("确认");
 		butSubmitAdd.setBounds(30, 100, 120, 40);
 		butSubmitAdd.setForeground(Color.BLUE);
@@ -87,13 +88,61 @@ public class UserInterface {
 		butCancelAdd.setBounds(150, 100, 120, 40);
 		addPanel.add(butSubmitAdd);
 		addPanel.add(butCancelAdd);
-			//窗体设置
+		//窗体设置
 		addFrame.setContentPane(addPanel);
 		addFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		addFrame.setVisible(false);
+	
+		/*
+		 * 新建编辑弹出框组件
+		 */
+		//新建窗体和面板
+		JFrame UpdateFrame = new JFrame("用户修改");
+		UpdateFrame.setBounds(600, 350, 300, 200);
+		JPanel UpdatePanel = new JPanel();
+		UpdatePanel.setLayout(null);
+		
+		//第一行——展示ID
+		JLabel labIdTip = new JLabel("ID为：");
+		labIdTip.setBounds(20,10, 40, 30);
+		JLabel labId = new JLabel();
+		labId.setBounds(90, 10, 150, 30);
+		UpdatePanel.add(labIdTip);
+		UpdatePanel.add(labId);
+		//第二行——用户名输入
+		JLabel labNameUpdate = new JLabel("用户名：");
+		labNameUpdate.setBounds(20, 40, 70, 30);
+		JTextField tfNameUpdate = new JTextField();
+		tfNameUpdate.setBounds(90, 40, 150, 30);
+		UpdatePanel.add(labNameUpdate);
+		UpdatePanel.add(tfNameUpdate);
+		//第三行——密码输入
+		JLabel labPasswordUpdate = new JLabel("密码：");
+		labPasswordUpdate.setBounds(20, 70, 70, 30);
+		JTextField tfPasswordUpdate = new JTextField();
+		tfPasswordUpdate.setBounds(90, 70, 150, 30);
+		UpdatePanel.add(labPasswordUpdate);
+		UpdatePanel.add(tfPasswordUpdate);
+		//第三行——提示语
+		JLabel labTipUpdate = new JLabel();
+		labTipUpdate.setBounds(90, 100, 200, 20);
+		labTipUpdate.setForeground(Color.RED);;
+		UpdatePanel.add(labTipUpdate);
+		//第四行——提交按钮
+		JButton butSubmitUpdate = new JButton("确认");
+		butSubmitUpdate.setBounds(30, 120, 120, 40);
+		butSubmitUpdate.setForeground(Color.BLUE);
+		JButton butCancelUpdate = new JButton("取消");
+		butCancelUpdate.setBounds(150, 120, 120, 40);
+		UpdatePanel.add(butSubmitUpdate);
+		UpdatePanel.add(butCancelUpdate);
+		//窗体设置
+		UpdateFrame.setContentPane(UpdatePanel);
+		UpdateFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		UpdateFrame.setVisible(false);
 		
 		/*
-		 * 动作模块——按钮动作
+		 * 动作模块——添加按钮动作
 		 */
 		butAdd.addActionListener(new ActionListener() {
 			
@@ -151,6 +200,106 @@ public class UserInterface {
 				});
 			}
 		});
+		
+		/*
+		 * 动作模块——删除按钮动作
+		 */
+		butDelete.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int selectInt = tableDisplay.getSelectedRow();
+				//判断是否选中的特定行
+				if(selectInt == -1) {
+					JOptionPane.showMessageDialog(frame, "请选择要删除的项目");
+					return;
+				}
+				int deleteConfirm = JOptionPane.showConfirmDialog(frame, "是否确认删除","确认",JOptionPane.YES_NO_OPTION);
+				if(deleteConfirm == JOptionPane.YES_OPTION) {
+					//获取选中行的id值
+					int selectId = (int) tableDisplay.getValueAt(selectInt,0);
+					//新建数据库对象，并执行删除方法
+					UserDAO userDao = new UserDAO();
+					userDao.delete(selectId);
+					//更新table model指针
+					utm.users = userDao.list();
+					//刷新页面
+					tableDisplay.updateUI();
+				}
+			}
+		});
+		
+		/*
+		 * 动作模块——编辑按钮
+		 */
+		butUpdate.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				UpdateFrame.setVisible(true);
+				UserDAO userDao = new UserDAO();
+				User userUpdate = new User();
+				//获取选中的项
+				int selectInt = tableDisplay.getSelectedRow();
+				//判断是否选中的特定行
+				if(selectInt == -1) {
+					JOptionPane.showMessageDialog(frame, "请选择要修改的项目");
+					return;
+				}
+				//获取选中行的id值
+				int selectId = (int) tableDisplay.getValueAt(selectInt,0);
+				String labIdString = selectId+"";
+				labId.setText(labIdString);
+				//判断用户是否确认修改
+				butSubmitUpdate.addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+
+						//获取输入框内容
+						String name = tfNameUpdate.getText();
+						String password = tfPasswordUpdate.getText();
+						//判断输入name是否为空
+						if(name.length() == 0) {
+							labTipUpdate.setText("用户名不能为空");
+							tfNameUpdate.grabFocus();
+							return;					
+						}
+						//判断密码是否为数字
+						try {
+							Integer.parseInt(password);
+						} catch (Exception e2) {
+							labTipUpdate.setText("密码需要为纯数字");
+							tfPasswordUpdate.grabFocus();
+							return;
+						}
+						//将输入的值赋给userUpdate对象
+						userUpdate.id = selectId;
+						userUpdate.name = name;
+						userUpdate.password = Integer.parseInt(password);
+						//调用更新方法
+						userDao.update(userUpdate);
+						//刷新指针
+						utm.users = userDao.list();
+						//刷新页面
+						tableDisplay.updateUI();
+						//关闭窗口
+						UpdateFrame.setVisible(false);
+					}
+				});
+				//判断用户是否取消添加
+				butCancelUpdate.addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						UpdateFrame.setVisible(false);						
+					}
+				});
+			}
+			
+		});
+		
+		
 		
 		
 		/*
